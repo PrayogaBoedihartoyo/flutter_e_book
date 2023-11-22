1a831d6e45bbc4ea1715a3e9b4ab835811823631
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-11-22 21:18:55 +0700

    Initial Commit

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -6,10 +6,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/profile.png'))),
+            ),
+            const SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello Sarah,',
+                  style: semiBoldText16,
+                ),
+                Text('Good Morning',
+                    style: regularText14.copyWith(color: greyColor)),
+              ],
+            ),
+            const Spacer(),
+            Image.asset('assets/icons/icon-menu.png', width: 18),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Home Page', style: semiBoldText20),
-      ),
-    );
+        backgroundColor: backgroundColor,
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  header(),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
