ee14c54741232cd9206ddb232af1648e862fa3ac
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-11-23 18:22:37 +0700

    Initial Commit

--- lib/screens/home/pages/book_detail.dart
+++ lib/screens/home/pages/book_detail.dart
@@ -1,11 +1,66 @@
+import 'package:e_book_app/themes.dart';
 import 'package:flutter/material.dart';
 
 class BookDetail extends StatelessWidget {
   static const nameRoute = '/bookDetail';
+
   const BookDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Widget header() {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        margin: const EdgeInsets.only(
+          top: 30,
+          bottom: 30,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(Icons.chevron_left_rounded, size: 30)),
+            Text(
+              'Book Detail',
+              style: semiBoldText14.copyWith(
+                color: blackColor,
+              ),
+            ),
+            const Icon(Icons.share_rounded),
+          ],
+        ),
+      );
+    }
+
+    Widget bookImage() {
+      return Container(
+        width: 275,
+        height: 375,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/trendingbook_1.png'),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              header(),
+              bookImage(),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
